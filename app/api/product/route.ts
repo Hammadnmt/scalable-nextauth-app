bd0d5ccddfd255478903e32fc4aba584022ad231
Hammad <hammad.alinmt@gmail.com>
2025-07-31 13:32:53 +0500

    deploying

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,9 +1,15 @@
 import { Product } from "@/model/Product";
 import connectDB from "@/config/connection";
-
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
 export async function GET(req: Request) {
   try {
     await connectDB();
+    const session = await getServerSession(authOptions);
+    console.log(session);
+    if (!session) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
     const products = await Product.find();
     return Response.json(products);
   } catch (err) {
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
 import Providers from "./provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
